b25855b333c46381902c809927942a0e8b869957
Sethador <159133251+Sethador@users.noreply.github.com>
2024-02-14 10:22:22 +0100

    Add files via upload

---
+++ PYTHON CODE 1/section1.py
@@ -0,0 +1,10 @@
+def print_menu():
+    print("Select an operation:")
+    print("1. Addition")
+    print("2. Subtraction")
+    print("3. Multiplication")
+    print("4. Division")
+    print("5. Exit")
+
+def add_numbers(num1, num2):
+    return num1 + num2
